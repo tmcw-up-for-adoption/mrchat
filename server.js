08fe4f163ebb8a6de123634543758c9dab699100
Tom MacWright <tom@macwright.org>
2014-02-25 11:14:10 -0500

    add backscroll

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app).listen(3000),
     shoe = require('shoe'),
+    fifo = require('stream-fifo'),
     through = require('through'),
     browserify = require('browserify-middleware');
 
 app.use('/js', browserify('./js'));
 app.use(express.static(__dirname + '/public'));
 
+var q = fifo(50);
 var combine = through(function write(data) {
     this.queue(data);
 });
 
 var sock = shoe(function(stream) {
-    stream.pipe(combine, { end: false }).pipe(stream, { end: false });
+    q.stack().pipe(stream, { end: false });
+    stream
+        .pipe(combine, { end: false })
+        .pipe(stream, { end: false })
+        .pipe(q);
 });
 
 sock.install(server, '/chat');
